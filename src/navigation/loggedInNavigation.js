11409f6db2b559b03c1b39517a55c6e196ee9a4e
fannyjian <97325835+fannyjian@users.noreply.github.com>
2022-06-24 14:35:46 +0800

    Updated navigations to wishlist screen

--- src/navigation/loggedInNavigation.js
+++ src/navigation/loggedInNavigation.js
@@ -11,7 +11,7 @@ const Tab = createBottomTabNavigator();
 
 export default function LoggedInNavigator() {
     return (
-        <Tab.Navigator 
+        <Tab.Navigator
             screenOptions={{headerShown:false, 
                             tabBarShowLabel: false, 
                             tabBarActiveTintColor:'white',
--- src/screens/wishlist.js
+++ src/screens/wishlist.js
@@ -41,6 +41,7 @@ export default function Wishlist() {
       });
     });
   }
+  
   useEffect(() => {
     loadItems();
   }, [])
